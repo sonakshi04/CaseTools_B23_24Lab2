603fdec5b7fe79df40592e9a60d774510c70bfcc
= <=>
2024-03-09 11:01:59 +0530

    comments

---
+++ lab3/src/main/java/bca2205112/bca2205112calculator.java
@@ -0,0 +1,18 @@
+package bca2205112;
+
+//Addition of two numbers 
+
+public class bca2205112calculator {
+    public int add(int a,int b)
+        /*
+         * 
+         * @param a [this is the first number in the addition]
+         * @param b [this is the second number in the addition]
+         * 
+         */
+    {
+        return a+b;  // @return the addition of two numbers a&b
+    }
+}
+
+
