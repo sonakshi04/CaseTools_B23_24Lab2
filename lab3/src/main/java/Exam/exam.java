af73bd0aa1d7b8806f4ce29f3775c99304a8bbb2
= <=>
2024-04-27 12:28:15 +0530

    exam file

---
+++ lab3/src/main/java/Exam/exam.java
@@ -0,0 +1,15 @@
+package Exam;
+/*
+ * This is a java code to calculate interest
+ */
+public class exam {
+    public double calculateInterest(double principal,double rate,int time)
+    {
+        /*
+         * @param principal [This is to substitute the principal value]
+         * @param rate [This is to substitute the value of rate]
+         * @param time [This is to substitute the value of time]
+         */
+        return principal*rate*time/100; //@return the interest
+    }
+}
